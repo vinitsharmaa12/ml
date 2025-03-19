71db777214bea1eefd26ac9c1880975df8e8ac75
vinit sharma <vinitsharmaa1205@gmail.com>
2025-03-19 19:46:02 +0530

    final registry files

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -11,7 +11,7 @@ import mlflow.sklearn
 import dagshub
 import os
 
-# Set up DagsHub credentials for MLflow tracking
+# #Set up DagsHub credentials for MLflow tracking
 # dagshub_token = os.getenv("DAGSHUB_PAT")
 # if not dagshub_token:
 #     raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
@@ -20,10 +20,10 @@ import os
 # os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 # dagshub_url = "https://dagshub.com"
-# repo_owner = "campusx-official"
-# repo_name = "mlops-mini-project"
+# repo_owner = "vinitsharmaa1205"
+# repo_name = "ml"
 
-# # Set up MLflow tracking URI
+# Set up MLflow tracking URI
 # mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 mlflow.set_tracking_uri('https://dagshub.com/vinitsharmaa1205/ml.mlflow')
 dagshub.init(repo_owner='vinitsharmaa1205', repo_name='ml', mlflow=True)
