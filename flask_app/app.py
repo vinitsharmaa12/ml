ca9e957acf9b41f7b2fbc9ee6d598a9316519954
vinit sharma <vinitsharmaa1205@gmail.com>
2025-03-24 17:06:52 +0530

    Flask workflow

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,7 +5,6 @@ import mlflow
 import pickle
 import os
 import pandas as pd
-import dagshub
 
 import numpy as np
 import pandas as pd
@@ -69,23 +68,20 @@ def normalize_text(text):
     return text
 
 
-mlflow.set_tracking_uri('https://dagshub.com/vinitsharmaa1205/ml.mlflow')
-dagshub.init(repo_owner='vinitsharmaa1205', repo_name='ml', mlflow=True)
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-# # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("DAGSHUB_PAT")
-# if not dagshub_token:
-#     raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+dagshub_url = "https://dagshub.com"
+repo_owner = "vinitsharmaa1205"
+repo_name = "ml"
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = 'vinitsharmaa1205'
-# repo_name = 'ml'
-
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 app = Flask(__name__)
 
